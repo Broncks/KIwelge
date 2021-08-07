565332d037203fae95dcf0abdc8bc6a1e105159a
Ollie <mollydoes@gmail.com>
2021-08-07 11:48:10 +0200

    asas

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #unsere main datei
 import tensorflow as tf
-#fmf
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+
 url = "https://myshare.leuphana.de/?t=dde5dfe5773fb088bd895a74b49933ab"
 tf.keras.utils.get_file("For_model", url, cache_dir="dataset", extract=True)
 
