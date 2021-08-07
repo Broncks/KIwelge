ac587dada00ea5fef1629c2ef7ef907cc3f1872b
Ollie <mollydoes@gmail.com>
2021-08-07 11:45:50 +0200

    asasf

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 #unsere main datei
+import tensorflow as tf
+#fmf
+url = "https://myshare.leuphana.de/?t=dde5dfe5773fb088bd895a74b49933ab"
+tf.keras.utils.get_file("For_model", url, cache_dir="dataset", extract=True)
+
