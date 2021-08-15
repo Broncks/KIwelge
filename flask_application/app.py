54f7a34973482a91e88f891f4d7b6b363e231b1d
david <davidleon.uhlenbrock@gmail.com>
2021-08-15 16:05:19 +0200

    add flask application

---
+++ flask_application/app.py
@@ -0,0 +1,36 @@
+import cv2
+from flask import Flask, render_template, Response
+
+app = Flask(__name__)
+camera = cv2.VideoCapture(0)
+clf = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+def gen_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = clf.detectMultiScale(gray, 1.1, 4)
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
