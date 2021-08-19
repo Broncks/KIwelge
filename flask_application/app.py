f6ccb5b87392eee32b5e92fcf0aa2d57391dd4f6
david <davidleon.uhlenbrock@gmail.com>
2021-08-18 21:07:07 -0700

    changes

--- flask_application/app.py
+++ flask_application/app.py
@@ -6,12 +6,15 @@ from tensorflow.keras.models import load_model
 from tensorflow.python.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 
-LABEL = ["maskchin", "maskmouth", "maskoff", "maskon"]
+LABEL = {0: "MASK OFF",
+         1: "MASK ON MOUTH",
+         2: "MASK OFF",
+         3: "MASK ON"}
 
 app = Flask(__name__)
 camera = cv2.VideoCapture(0)
 clf = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-model = load_model('mask_detector_FineTuned.model')
+model = load_model('mask_detector_aug3.model')
 
 
 @app.route('/')
@@ -30,10 +33,6 @@ def gen_frames():
         if not success:
             break
         else:
-            # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            # faces = clf.detectMultiScale(gray, 1.1, 4)
-            # for (x, y, w, h) in faces:
-            #     cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
             frame = detect_mask(frame)
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
@@ -46,17 +45,24 @@ def detect_mask(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = clf.detectMultiScale(gray, 1.1, 4)
     faces_dict = {"faces_list": [], "faces_rect": []}
+
     for rect in faces:
         (x, y, w, h) = rect
         face_frame = frame[y:y + h, x:x + w]
         face_frame_prepared = preprocess_face_frame(face_frame)
         faces_dict["faces_list"].append(face_frame_prepared)
         faces_dict["faces_rect"].append(rect)
+
     if faces_dict["faces_list"]:
         faces_preprocessed = preprocess_input(np.array(faces_dict["faces_list"]))
         preds = model.predict(faces_preprocessed)
-        print(LABEL[np.argmax(preds)])
-        # TODO: show label with frame on screen
+
+        for i, pred in enumerate(preds):
+            label = LABEL[np.argmax(pred)]
+            (x, y, w, h) = faces_dict["faces_rect"][i]
+            cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
     return frame
 
 
